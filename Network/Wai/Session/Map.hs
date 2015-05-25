2321f0b12ad46e3256078f77f68c590241e95d03
Owen Smith <ods94043@yahoo.com>
2015-05-24 18:13:01 -0700

    Eliminate GHC tab warnings

--- Network/Wai/Session/Map.hs
+++ Network/Wai/Session/Map.hs
@@ -19,23 +19,23 @@ import qualified Data.Map as Map
 -- WARNING: This session is vulnerable to sidejacking,
 -- use with TLS for security.
 mapStore :: (Ord k, MonadIO m) =>
-	IO ByteString
-	-- ^ 'IO' action to generate unique session IDs
-	-> IO (SessionStore m k v)
+    IO ByteString
+    -- ^ 'IO' action to generate unique session IDs
+    -> IO (SessionStore m k v)
 mapStore gen =
-	liftM (mapStore' gen) (newThreadSafeStateVar Map.empty)
-	where
-	mapStore' _ ssv (Just k) = do
-		m <- get ssv
-		case Map.lookup k m of
-			Just sv -> return (sessionFromMapStateVar sv, return k)
-			-- Could not find key, so it's as if we were not sent one
-			Nothing -> mapStore' (return k) ssv Nothing
-	mapStore' genNewKey ssv Nothing = do
-		newKey <- genNewKey
-		sv <- newThreadSafeStateVar Map.empty
-		ssv $~ Map.insert newKey sv
-		return (sessionFromMapStateVar sv, return newKey)
+    liftM (mapStore' gen) (newThreadSafeStateVar Map.empty)
+    where
+    mapStore' _ ssv (Just k) = do
+        m <- get ssv
+        case Map.lookup k m of
+            Just sv -> return (sessionFromMapStateVar sv, return k)
+            -- Could not find key, so it's as if we were not sent one
+            Nothing -> mapStore' (return k) ssv Nothing
+    mapStore' genNewKey ssv Nothing = do
+        newKey <- genNewKey
+        sv <- newThreadSafeStateVar Map.empty
+        ssv $~ Map.insert newKey sv
+        return (sessionFromMapStateVar sv, return newKey)
 
 -- | Store using simple session ID generator based on time and 'Data.Unique'
 mapStore_ :: (Ord k, MonadIO m) => IO (SessionStore m k v)
@@ -43,20 +43,20 @@ mapStore_ = mapStore genSessionId
 
 newThreadSafeStateVar :: a -> IO (StateVar a)
 newThreadSafeStateVar a = do
-	ref <- newIORef a
-	return $ makeStateVar
-		(atomicModifyIORef ref (\x -> (x,x)))
-		(\x -> atomicModifyIORef ref (const (x,())))
+    ref <- newIORef a
+    return $ makeStateVar
+        (atomicModifyIORef ref (\x -> (x,x)))
+        (\x -> atomicModifyIORef ref (const (x,())))
 
 #if MIN_VERSION_StateVar(1,1,0)
 sessionFromMapStateVar :: (Ord k, MonadIO m) =>
-	StateVar (Map k v) ->
+    StateVar (Map k v) ->
 #else
 sessionFromMapStateVar :: (HasGetter sv, HasSetter sv, Ord k, MonadIO m) =>
-	sv (Map k v) ->
+    sv (Map k v) ->
 #endif
-	Session m k v
+    Session m k v
 sessionFromMapStateVar sv = (
-		(\k -> liftIO (Map.lookup k `liftM` get sv)),
-		(\k v -> liftIO (sv $~ Map.insert k v))
-	)
+        (\k -> liftIO (Map.lookup k `liftM` get sv)),
+        (\k v -> liftIO (sv $~ Map.insert k v))
+    )
